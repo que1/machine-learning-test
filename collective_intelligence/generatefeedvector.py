2b427c4c713b346c7bcc9599a746ba6e8f5541e7
que1 <que1@163.com>
2017-11-23 17:27:39 +0800

    20171123_1

--- collective_intelligence/generatefeedvector.py
+++ collective_intelligence/generatefeedvector.py
@@ -16,10 +16,10 @@ def getwordcounts(url):
         for word in words:
             wc.setdefault(word,0)
             wc[word]+=1
-    return d.feed.title,wc
+    return d.feed.title, wc
 
 def getwords(html):
-    # Remove all the HTML tags
+    # Remove all the HTML tagsgit config --global user.email "you@example.com"
     txt = re.compile(r'<[^>]+>').sub('',html)
 
     # Split words by all non-alpha characters
@@ -28,8 +28,45 @@ def getwords(html):
     # Convert to lowercase
     return [word.lower() for word in words if word!='']
 
-
-
-
 if __name__ == '__main__':
-    title, wc = getwordcounts("https://kiwitobes.com")
+    apcount = {}
+    wordcounts = {}
+    feedlist = ["http://blog.csdn.net/xiaoquantouer/rss/list",
+                "http://blog.csdn.net/sunhuaqiang1/rss/list",
+                "http://blog.csdn.net/rickiyeat/rss/list",
+                "http://blog.csdn.net/qq_40027052/rss/list"]
+
+    for feedurl in feedlist:
+        title, wc = getwordcounts(feedurl)
+        wordcounts[title] = wc
+        for word, count in wc.items():
+            apcount.setdefault(word, 0)
+            if count > 1:
+                apcount[word] += 1
+
+    print(wordcounts)
+    print(apcount)
+
+    wordlist = []
+    for w, bc in apcount.items():
+        frac = float(bc)/len(feedlist)
+        if frac > 0.1 and frac < 0.5:
+            wordlist.append(w)
+    print(wordlist)
+
+    out = open('/Users/q/program/blogdata.txt', 'w')
+    out.write("Blog")
+    
+    for word in wordlist:
+        out.write("\t%s" % word)
+    out.write("\n")
+    for blog, wc in wordcounts.items():
+        out.write(blog)
+        for word in wordlist:
+            if word in wc:
+                out.write("\t%s" % wc[word])
+            else:
+                out.write("\t0")
+        out.write("\n")
+
+    out.close()
